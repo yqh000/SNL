b0cd8221ed1b29d7301334cb2b27b30824f69cb8
LiYonghao1204 <50099100+LiYonghao1204@users.noreply.github.com>
2020-06-14 21:15:50 +0800

    Create Process.java

---
+++ src/grammar/Process.java
@@ -0,0 +1,10 @@
+package grammar;
+
+public class Process {
+	private UtilLL1 ul = new UtilLL1();
+	
+		ul.push(1, NonTerminals.ProcDecpart);
+		ul.push(1, NonTerminals.VarDecpart);
+		ul.push(1, NonTerminals.TypeDecpart);
+	}
+}
